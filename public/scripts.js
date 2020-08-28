50788f89395450e47600385595b32c9ed7b9b572
Gerberson de Souza Crisostomo Dias  [e] <gdias@fitec.org.br>
2020-08-28 11:11:30 -0300

    Atualizando mascara de moeda

--- public/scripts.js
+++ public/scripts.js
@@ -1,13 +1,10 @@
-const input = document.querySelector('input[name=price]')
-input.addEventListener("keydown", function(e) {
-    setTimeout(() => {
-        let { value } = e.target
+const Mask = {
+    apply(input, func) {
+        setTimeout(function() {
+            input.value = Mask[func](value)
+        }, 1)
+    },
+    formatBRL(value) {
 
-        value = value.replace(/\D/g, "")
-
-        value = new Intl.NumberFormat('pt-BR', {
-            style: 'currency',
-            currency: 'BRL'
-        }).format(value / 100)
-    })
-})
+    }
+}
