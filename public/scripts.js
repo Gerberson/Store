901cff1cc555f249ca7ebe7c1c5add09bc94e631
Gerberson Dias <gerbersondias.312@gmail.com>
2020-08-30 16:23:39 -0300

    Implementando logica do fotos preview

--- public/scripts.js
+++ public/scripts.js
@@ -15,15 +15,47 @@ const Mask = {
 }
 
 const PhotosUpload = {
+    preview: querySelector('#photos-preview'),
     uploadLimit: 6,
     handleFileInput(event) {
         const { files: fileList } = event.target
+        
+        if(PhotosUpload.hasLimit(event)) return
+
+        Array.from(fileList).forEach(file => {
+            const reader = new FileReader()
+
+            reader.onload = () => {
+                const image = new Image()
+                image.src = String(reader.result)
+                
+                const div = PhotosUpload.getContainer(image)
+
+                PhotosUpload.preview.appendChild(div)
+            }
+
+            reader.readAsDataURL(file)
+        })
+    },
+    getContainer(image) {
+        const div = document.createElement('div')
+        div.classList.add('photos')
+
+        div.onclick = () => alert('Remover')
+        div.appendChild(image)
+
+        return div
+    },
+    hasLimit(event) {
         const { uploadLimit } = PhotosUpload
+        const { files: fileList } = event.target
 
         if (fileList.length > uploadLimit) {
             alert(`Envie no máximo ${ uploadLimit }`)
             event.preventDefault()
-            return
+            return true
         }
+
+        return false
     }
 }
